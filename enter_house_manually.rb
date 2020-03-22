5ffd2317179903ef987e03926853410aa81948d5
marcus-elia <mselia@uvm.edu>
2020-03-21 20:38:49 -0400

    Added some prompts to get house data from user input.

--- enter_house_manually.rb
+++ enter_house_manually.rb
@@ -28,14 +28,47 @@ def format_road_name(road_name)
   elsif road_name.end_with("Trail")
     road_name.gsub!(/Trail/, "Trl")
   return road_name
-  
 
-# Get the data from the house through user input
+def string_to_boolean(s)
+  return s.downcase == "true"
+  
+# =======================
+#
+#       User Input
+#
+# =======================
+
+# Get the name of the house in 3 parts
 house_name = ""
 print "Enter the house number: "
 house_number = gets.chop 
 print "Enter the road name: "
-road_name = gets.chop
+road_name = format_road_name(gets.chop)
 print "Enter the ZIP code: "
 zip_code = gets.chop
-puts house_number + " " + road_name + " " + zip_code
+house_name = house_number + " " + road_name + " " + zip_code
+
+# A hash that will store the house's data
+data2value = {}
+
+# Prompt for each thing, and add them to the hash
+print "How much does this house cost? "
+data2value["Price"] = gets.chop.to_i
+print "How many bedrooms? "
+data2value["Bedrooms"] = gets.chop.to_i
+print "How many square feet? "
+data2value["Square Feet"] = gets.chop.to_i
+print "How many bathrooms? "
+data2value["Bathrooms"] = gets.chop.to_f
+print "How many cars fit in the garage? "
+data2value["Garage"] = gets.chop.to_i
+print "What year is it from? "
+data2value["Year"] = gets.chop.to_i
+print "How many acres is the lot? "
+data2value["Lot"] = gets.chop.to_f
+print "Is it in a neighborhood? Enter true or false: "
+data2value["Neighborhood"] = string_to_boolean(gets.chop)
+
+
+
+
